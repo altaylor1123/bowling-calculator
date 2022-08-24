ab1edd2061def4c7ad5c280edebbf1f20457f7a5
Alex Taylor <altaylor1123@gmail.com>
2022-08-24 07:21:37 -0500

    exploring more functional patterns

--- calculateBowl.js
+++ calculateBowl.js
@@ -1,54 +1,48 @@
 function calculateBowl(rolls) {
-  if (rolls.length === 0) return [];
-  let index = 0;
-  const score = [];
+  let score = [];
+  if (rolls.length === 0) return score;
 
-  const isSpare = (roll) => {
-    return roll === "/";
-  };
-  const isStrike = (roll) => {
-    return roll === "X";
-  };
-
-  const calculateStrikeFrame = () => {
-    if (rolls[index + 1] === undefined || rolls[index + 2] === undefined)
-      return null;
-    return rolls.slice(index + 1, index + 3).reduce((acc, roll, _, array) => {
-      if (isSpare(roll)) {
-        return acc + (10 - array[0]);
-      }
-      if (isStrike(roll)) {
-        return acc + 10;
-      }
-      return acc + roll;
-    }, 10);
-  };
-
-  const calculateFrame = () => {
-    if (rolls[index + 1] === undefined) return null;
-    return rolls.slice(index, index + 2).reduce((acc, roll, _, array) => {
-      if (isSpare(roll)) {
-        return rolls[index + 2] !== undefined && rolls[index + 3] !== undefined
-          ? acc + (10 - array[0]) + rolls[index + 2]
-          : null;
-      }
-      return acc + roll;
-    }, 0);
-  };
-
-  while (index < rolls.length) {
-    if (isStrike(rolls[index])) {
-      const frameScore = calculateStrikeFrame();
-      score.push(frameScore);
-      index++;
-    } else {
-      const frameScore = calculateFrame();
-      score.push(frameScore);
-      index += 2;
-    }
+  if (isStrike(rolls[0])) {
+    const frameScore = calculateStrikeFrame(rolls);
+    score.push(frameScore);
+    return score.concat(calculateBowl(rolls.slice(1)));
+  } else {
+    const frameScore = calculateFrame(rolls);
+    score.push(frameScore);
+    return score.concat(calculateBowl(rolls.slice(2)));
   }
-
-  return score;
 }
+
+const isSpare = (roll) => {
+  return roll === "/";
+};
+const isStrike = (roll) => {
+  return roll === "X";
+};
+
+const calculateStrikeFrame = (rolls) => {
+  if (rolls[1] === undefined || rolls[2] === undefined) return null;
+  return rolls.slice(1, 3).reduce((acc, roll, _, array) => {
+    if (isSpare(roll)) {
+      return acc + (10 - array[0]);
+    }
+    if (isStrike(roll)) {
+      return acc + 10;
+    }
+    return acc + roll;
+  }, 10);
+};
+
+const calculateFrame = (rolls) => {
+  if (rolls[1] === undefined) return null;
+  return rolls.slice(0, 2).reduce((acc, roll, _, array) => {
+    if (isSpare(roll)) {
+      return rolls[2] !== undefined && rolls[3] !== undefined
+        ? acc + (10 - array[0]) + rolls[2]
+        : null;
+    }
+    return acc + roll;
+  }, 0);
+};
 
 module.exports = calculateBowl;
